7df703934fd18733079a826a5ad9850d02b85899
zhzhz <932496816@qq.com>
2019-12-03 15:13:43 +0800

    初始化main

--- main.cpp
+++ main.cpp
@@ -7,13 +7,6 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::vector<int> v = { 1, 2, 3 };
-    std::list<int> l = { 1, 2, 3 };
-    std::set<int> s = { 1, 2, 3 };
-    std::map<int, std::string> m = { {1, "a"}, {2, "b"} };
 
-    qDebug() << "hello qt";
-    class1 class11;
-    class2 class22;
     return a.exec();
 }
