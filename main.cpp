370d2439fe8715763b67b9b931716cafc70264d0
zhzhz <932496816@qq.com>
2019-12-03 10:42:32 +0800

    base

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include "set"
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    std::vector<int> v = { 1, 2, 3 };
+    std::list<int> l = { 1, 2, 3 };
+    std::set<int> s = { 1, 2, 3 };
+    std::map<int, std::string> m = { {1, "a"}, {2, "b"} };
+    return a.exec();
+}
